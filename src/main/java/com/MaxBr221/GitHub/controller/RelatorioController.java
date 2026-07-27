79e0e4dba1ede73b45193bdd7808d7e271de9bbb
Maxsuel <maxsuel.lima@dcx.ufpb.br>
2026-07-27 11:41:47 -0300

    feat - RelatorioController

---
+++ src/main/java/com/MaxBr221/GitHub/controller/RelatorioController.java
@@ -0,0 +1,38 @@
+package com.MaxBr221.GitHub.controller;
+
+import com.MaxBr221.GitHub.dtos.relatorioDTO.RelatorioResponseDTO;
+import com.MaxBr221.GitHub.service.RelatorioService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/relatorio")
+@RequiredArgsConstructor
+@Slf4j
+public class RelatorioController {
+    private final RelatorioService relatorioService;
+
+    @GetMapping("/diario")
+    public ResponseEntity<RelatorioResponseDTO> relatorioDiario(){
+        RelatorioResponseDTO relatorioResponseDTO = relatorioService.relatorioDiario();
+        log.info("Listando relatorio diario!");
+        return ResponseEntity.ok(relatorioResponseDTO);
+    }
+    @GetMapping("/mensal")
+    public ResponseEntity<RelatorioResponseDTO> relatorioMensal(){
+        RelatorioResponseDTO relatorioResponseDTO = relatorioService.relatorioMensal();
+        log.info("Listando relatorio mensal!");
+        return ResponseEntity.ok(relatorioResponseDTO);
+    }
+    @GetMapping("/anual")
+    public ResponseEntity<RelatorioResponseDTO> relatorioAnual(){
+        RelatorioResponseDTO relatorioResponseDTO = relatorioService.relatorioAnual();
+        log.info("Listando relatorio anual!");
+        return ResponseEntity.ok(relatorioResponseDTO);
+    }
+
+}
