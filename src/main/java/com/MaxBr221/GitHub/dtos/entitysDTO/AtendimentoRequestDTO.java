c700f5b8fb25d74a162ddc82ec5245847eeefa1d
Maxsuel <maxsuel.lima@dcx.ufpb.br>
2026-08-29 18:22:11 -0300

    refactor - deletando parametro não usavel de AtendimentoRequest

--- src/main/java/com/MaxBr221/GitHub/dtos/entitysDTO/AtendimentoRequestDTO.java
+++ src/main/java/com/MaxBr221/GitHub/dtos/entitysDTO/AtendimentoRequestDTO.java
@@ -5,7 +5,6 @@ import com.MaxBr221.GitHub.model.FormaPagamento;
 import java.util.List;
 
 public record AtendimentoRequestDTO(
-        Long usuarioId,
         FormaPagamento formaPagamento,
         String observacao,
         List<Long> servicosIds
